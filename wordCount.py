132fda16c927b2b5bb244d3f48798d4153116491
sithlord33 <ricardotodd33@gmail.com>
2018-09-03 22:07:36 -0600

    added the input and output files for the aguments

--- wordCount.py
+++ wordCount.py
@@ -3,7 +3,30 @@ import re
 import os
 import subprocess
 
+#sentence = "Hello, world!"
+#print sentence
+#print len(sentence)
 
-sentence = "Hello, world!"
-print sentence
-print len(sentence)
+inFileName = sys.argv[1]
+outFileName = sys.argv[2]
+
+f = open(inFileName, "r+")
+
+contents = f.read()
+
+words = contents.split()
+actual = ["none"] * 10
+
+for i in words:
+    for j in actual:
+        if i == j:
+            #actual.append(i)
+
+print len(actual)
+
+for x in range(len(actual)):
+    print actual(x)
+    
+f = open(outFileName, "w+")
+
+#f.write(actual)
