599f0cf069c5cc651cb8acf245eae949fb5ec182
sithlord33 <ricardotodd33@gmail.com>
2018-09-17 18:02:42 -0600

    shell directory

--- wordCount.py
+++ wordCount.py
@@ -18,17 +18,25 @@ contents = f.read()
 #list of words in the input file
 words = contents.split()
 #list of words not repeated
-actual = []
+actual = ["init"]
+
+x = 0
 
 #check of word is already in list or add it
-for i in words:
-    for j in actual:
-        if i != j:
-            actual.append(i)
+for i in range(len(words)):
+    for j in range(len(actual)):
+        if words[i] != actual[j]:
+            actual.append(words[i])
 
-#print(*words, sep = "\n"),
-print('\n'.join(map(str, words)))
+print len(actual)
+
+#print list using maps
+#print('\n'.join(map(str, words)))
     
-f = open(outFileName, "w+")
+#f = open(outFileName, "w+")
+
+with open(outFileName, 'w') as f:
+    for item in actual:
+        print >> thefile, item
 
 #f.write(actual)
